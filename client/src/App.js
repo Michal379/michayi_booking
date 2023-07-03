84fb8e063619ecd66dc58b028c5d3b18b2af6382
Michal <khakaimic4@gmail.com>
2023-07-03 21:26:37 +0300

    created single hotel

--- client/src/App.js
+++ client/src/App.js
@@ -1,6 +1,7 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
-import Home from "./pages/home/Home";
+import Home from "./pages/components/Home";
 import List from "./pages/List";
+import Hotel from "./pages/components/Hotel";
 
 function App() {
   return (
@@ -8,6 +9,7 @@ function App() {
     <Routes>
       <Route path="/" element={<Home />} />
       <Route path="/hotels" element={<List />} />
+      <Route path="/hotels/ :id" element={<Hotel />} />
     </Routes>    
     </BrowserRouter>
   );
