c1c8b1dc49c8204af3621e757d3728217775bb1b
Michal <khakaimic4@gmail.com>
2023-07-03 21:21:33 +0300

    created hotels

--- client/src/App.js
+++ client/src/App.js
@@ -1,11 +1,13 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Home from "./pages/home/Home";
+import List from "./pages/List";
 
 function App() {
   return (
     <BrowserRouter>
     <Routes>
       <Route path="/" element={<Home />} />
+      <Route path="/hotels" element={<List />} />
     </Routes>    
     </BrowserRouter>
   );
