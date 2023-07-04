1275f04836d69246550f4a2fde1cefce00f8d499
Michal <khakaimic4@gmail.com>
2023-07-04 12:01:50 +0300

    searchbar

--- client/src/pages/components/Header.jsx
+++ client/src/pages/components/Header.jsx
@@ -2,19 +2,23 @@ import React, { useState } from 'react'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import { faHotel } from '@fortawesome/free-solid-svg-icons'
 import { faCalendarDays } from '@fortawesome/free-solid-svg-icons'
+import { faPeopleGroup } from '@fortawesome/free-solid-svg-icons'
 import { DateRange } from 'react-date-range';
 import 'react-date-range/dist/styles.css'; // main css file
 import 'react-date-range/dist/theme/default.css'; // theme css file
+import { format } from 'date-fns'
 
 
 const Header = () => {
+    const [openDate, setOPenDate] = useState(false)
     const [date, setDate] = useState([
         {
           startDate: new Date(),
-          endDate: null,
+          endDate: new Date(),
           key: 'selection'
         }
       ]);
+    //   const [openOptions, setOPenOPtions] = useState(false)
 
   return (
     <div className='header'>
@@ -37,13 +41,19 @@ const Header = () => {
             </div>
             <div className='headerSearchItem'>
             <FontAwesomeIcon icon={faCalendarDays} className='headerIcon' />
-            <span className='headerSearchText'>date to date</span> 
-            <DateRange
+            <span onClick={()=>setOPenDate(!openDate)} className='headerSearchText'>{`${format(date[0].startDate, "MM/dd/yyyy")} to ${format(date[0].endDate, "MM/dd/yyyy")}`}</span> 
+            {openDate && <DateRange
                 editableDateInputs={true}
                 onChange={item => setDate([item.selection])}
                 moveRangeOnFirstSelection={false}
                 ranges={date}
-            />          
+                className='date'
+            /> }         
+            </div>
+            <div className='headerSearchItem'>
+            <FontAwesomeIcon icon={faPeopleGroup} className='headerIcon' />
+            <input type='text' placeholder='guests' 
+            className='headerSearchInput' />
             </div>
             <div className='headerSearchItem'>
                 <button className='headerBtn'>Search</button>
