74ca4080c26061ba90fba5003ccea328fc417a9e
Michal <khakaimic4@gmail.com>
2023-07-05 04:08:02 +0300

    users controller

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,38 +1,40 @@
 class UsersController < ApplicationController
-    def index
-        users = User.all
-        render json: users.to_json(except: [:created_at, :updated_at])
-    end
     
+    def index
+      users = User.all
+      render json: users.to_json(except: [:created_at, :updated_at])
+    end
+  
     def show
-        user = find_user
-        render json: user.to_json(except: [:created_at, :updated_at])
+      user = find_user
+      render json: user.to_json(except: [:created_at, :updated_at])
     end
-
+  
     def create
-        user = user.create(user_params)
-        render json: user, status: :created
+      user = User.create(user_params)
+      render json: user, status: :created
     end
-
+  
     def update
-        user = find_user
-        user.update(user_params)
-        render json: user
+      user = find_user
+      user.update(user_params)
+      render json: user.to_json(except: [:created_at, :updated_at])
     end
-
+  
     def destroy
-        user =find_user
-        user.destroy
-        head :no_content
+      user = find_user
+      user.destroy
+      head :no_content
     end
-
+  
     private
-
+  
     def find_user
-        user.find_by(id: params[:id])
+      User.find_by(id: params[:id])
     end
-
+  
     def user_params
-        params.permit(:name, :email, :age, :nation, :phone_number, :password)
+      params.permit(:name, :email, :age, :nation, :phone_number, :password)
     end
-end
+  end
+  
