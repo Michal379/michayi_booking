9db00b602fa4fdf5624845ada1ae7bf78caa975b
Michal <khakaimic4@gmail.com>
2023-07-09 04:13:49 +0300

    details page

--- client/src/pages/components/BookingDetailsPage.jsx
+++ client/src/pages/components/BookingDetailsPage.jsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import { useLocation } from 'react-router-dom';
+import '../../App.css';
 
 const BookingDetailsPage = ({ userDetails }) => {
   const location = useLocation();
@@ -10,19 +11,18 @@ const BookingDetailsPage = ({ userDetails }) => {
   }
 
   return (
-    <div>
+    <div className="booking-details-container" style={{ backgroundImage: `url('https://cf.bstatic.com/xdata/images/hotel/max1024x768/342980112.jpg?k=e6be6ff1b99a74127bc30a32ae5dcb4043d8d88d04b34f73e4af66b0edf2615f&o=&hp=1')`, backgroundSize: 'cover', backgroundPosition: 'center', backgroundRepeat: 'no-repeat' }}>
       <h1>Booking Details</h1>
       <h2>Hotel: {hotel?.name}</h2>
       <h3>User Details:</h3>
-      <p>Name: {userDetails?.name }</p>
-      <p>Email: {userDetails?.email }</p>
-      <p>Phone: {userDetails?.phone_number }</p>
+      <p>Name: {userDetails?.name}</p>
+      <p>Email: {userDetails?.email}</p>
+      <p>Phone: {userDetails?.phone_number}</p>
       <h3>Room Details:</h3>
       <p>Type: {roomType}</p>
       <p>Price: {roomPrice}</p>
     </div>
   );
 };
-
 
 export default BookingDetailsPage;
